bf4f2cbefcc436348fcdd486edd7cbeb57fe68aa
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-26 11:35:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic1
+{
+    class example
+    {
+        public static void details<t>(t[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr ={11,2,3,55,66};
+            string[] name = {"hrishi","rahit","rehan" };
+            char[] code = { 'A', 'b', 'C' };
+            example.details(name);
+            example.details(arr);
+            example.details(code);
+        }
+    }
+}
